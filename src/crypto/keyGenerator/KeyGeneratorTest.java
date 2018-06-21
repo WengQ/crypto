aaae31825d93aa830cbd4322b77587e308f5bf0b
wengqi <916949507@qq.com>
2018-06-21 09:53:00 +0800

    常见用法

--- src/crypto/keyGenerator/KeyGeneratorTest.java
+++ src/crypto/keyGenerator/KeyGeneratorTest.java
@@ -2,22 +2,33 @@ package crypto.keyGenerator;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
 import java.security.NoSuchAlgorithmException;
 
 public class KeyGeneratorTest {
-    //实例化keyGenerator对象并且指定算法。
-    KeyGenerator keyGenerator;
+    public static void main(String[] args) throws Exception{
 
-    {
-        try {
-            keyGenerator = KeyGenerator.getInstance("HmacMD5");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+        //实例化keyGenerator对象并且指定算法。
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
+        //使用密钥生成器产生SecretKey对象
+        SecretKey secretKey = keyGenerator.generateKey();
+        //获得秘密密钥的密钥编码字节数组
+        byte[] key = secretKey.getEncoded();
+        System.out.println(secretKey);
+
+        //---------密钥的字节数组可能用于传输之类的
+        //---------接收方可以根据密钥的字节数组 使用 SecretKeyFactory来还原秘密密钥
+
+        //构建DESKeySpec对象
+        DESKeySpec desKeySpec = new DESKeySpec(key);
+        //实例化SecretKeyFactory对象
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DES");
+        //生成SecretKey对象
+        SecretKey secretKey1 = secretKeyFactory.generateSecret(desKeySpec);
+        //打印出来的secretKey对象和编码前的是同一个对象。
+        System.out.println(secretKey1);
     }
-
-    //使用密钥生成器产生SecretKey对象
-    SecretKey secretKey = keyGenerator.generateKey();
 
 
 }
