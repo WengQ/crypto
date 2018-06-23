a42c5087d68ecaac9cf50b4d464852bcd07ad882
wengqi <916949507@qq.com>
2018-06-24 02:05:16 +0800

    常见用法

--- src/base64/Base64Example.java
+++ src/base64/Base64Example.java
@@ -1,5 +1,6 @@
 package base64;
 
+import org.apache.commons.codec.binary.Base64;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
@@ -9,9 +10,11 @@ public class Base64Example {
     private static String src = "Base64 encode using different ways";
 
     public static void main(String[] args) throws Exception{
-        jdkBase64();
+        //jdkBase64();
+        //commonsCodecBase64();
+        bouncyCastleBase64();
     }
-
+    //JDK的方法完成Base64的加解密
     public static void jdkBase64() throws IOException {
         BASE64Encoder encoder = new BASE64Encoder(); //字符串要使用getBytes()方法变成字节数组才能被编码
         String code = encoder.encode(src.getBytes());//encode方法把字节数组编码成base64字符串
@@ -20,6 +23,25 @@ public class Base64Example {
         BASE64Decoder decoder = new BASE64Decoder();
         byte[] plainText = decoder.decodeBuffer(code);//decodeBuffer方法把字符串解码成字节数组
         //打印字节数组用String类构造方法即可
+        System.out.println("base64 decode:" + new String(plainText));
+    }
+
+    //CommonsCodec的方式完成Base64的加解密
+    public static void commonsCodecBase64() {
+        //不同之处：encodeBase64()返回值还是byte[],若想返回字符串则使用encodeBase64String()
+        byte[] code = Base64.encodeBase64(src.getBytes());
+        System.out.println("base64 encode:" + new String(code));
+
+        byte[] plainText = Base64.decodeBase64(code);
+        System.out.println("base64 decode:" + new String(plainText));
+    }
+
+    //CommonsCodec的方式完成Base64的加解密
+    public static void bouncyCastleBase64() {
+        byte[] code = org.bouncycastle.util.encoders.Base64.encode(src.getBytes());
+        System.out.println("base64 encode:" + new String(code));
+
+        byte[] plainText = org.bouncycastle.util.encoders.Base64.decode(code);
         System.out.println("base64 decode:" + new String(plainText));
     }
 }
--- src/base64/base64.md
+++ src/base64/base64.md
@@ -3,3 +3,8 @@
 - JDK 是Oracle（Sun）公司实现的
 - Commons Codec 是Apache旗下的一款开源软件
 - Bouncy Castle
+
+建议使用后两种方法，实现更简单。
+需要注意的是Commons Codec 和 Bouncy Castle 
+对于Base64编解码的实现方式并不一样。
+Base64本身没有编解码的标准，所以不可混用，具体需要根据实际场景选择。
